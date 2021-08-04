dc936ac9e6eeba8994d95cae77d4e0dd2929c880
Ashley Brown <ashley.brown1@ntgfreight.com>
2021-08-04 13:31:29 -0500

    added components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,21 +10,42 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './_components/home/home.component';
 import { RegisterComponent } from './_components/register/register.component';
+import { MemberListComponent } from './_components/member-list/member-list.component';
+import { MemberDetailComponent } from './_components/member-detail/member-detail.component';
+import { ListsComponent } from './_components/lists/lists.component';
+import { MessagesComponent } from './_components/messages/messages.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'members', component: MemberListComponent},
+  {path: 'members/:id', component: MemberDetailComponent},
+  {path: 'lists', component: ListsComponent},
+  {path: 'messages', component: MessagesComponent},
+  {path: '**', component: HomeComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule, 
     BsDropdownModule.forRoot()
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
